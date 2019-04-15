ba4abfa6b03b51aaf336f75954991302490cbf5c
Alexandre Grenier <grenieralexandre@free.fr>
2019-04-15 18:06:33 +0200

    0.2.0 : Adaptation au nouveau servo

--- main/main.ino
+++ main/main.ino
@@ -6,7 +6,7 @@
 //                                   //
 // **** STABILISATEUR DE CAMERA **** //
 //                                   //
-// ********* Version 0.1.3 ********* //
+// ********* Version 0.2.0 ********* //
 
 // Inclusion des librairies //
 #include <Wire.h> // Librairie pour le controle des ports analogiques
@@ -18,11 +18,11 @@ Servo interfaceMoteur ;  // Instance de Servo pour controler le moteur
 // Declaration des variables globales
 float u = 0; // Variable post-filtre passe bas
 float tempsCaracteristique = 0.1; // Pulsation de coupure du filtre passe-bas (rad/s)
-float pause = 20; // Temps de pause entre deux loop (ms)
-float gain = 0.01; // Gain de l'asservissement
+float pause = 10; // Temps de pause entre deux loop (ms)
+float gain = 0.1; // Gain de l'asservissement
 const int MPU_addr=0x68;  // Adresse I2C du MPU-6050
-float AcX; // Composante sur X
-float marge = 5; 
+float AcX,AcY,AcZ; // Composante sur X
+float marge = 1; 
 
 
 void setup()
@@ -50,25 +50,28 @@ void loop()
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)    
   
 	// Sortie de controle //
 	Serial.print("U : ");
 	Serial.print(u);
 	Serial.print(" , X : ");
-  Serial.println(AcX/100);
+  Serial.println(AcY/100);
   // ****************** //
 
   // Filtrage passe-bas de x par la methode d'Euler
-  u = u +(pause/1000)*(AcX/(100*tempsCaracteristique) - u/tempsCaracteristique);
+  u = u +(pause/1000)*(AcY/(100*tempsCaracteristique) - u/tempsCaracteristique);
 
-  if(-1*(marge/2) < gain*u < (marge/2))
+  /*if(-1*(marge/2) < gain*u < (marge/2))
   {
     u = 0;
-  };
+  };*/
 
   // Asservissement de la vitesse du moteur
-  interfaceMoteur.write(90+gain*u); // 90 => Vitesse nulle
-	
+  interfaceMoteur.write(float(90-gain*u)); // 90 => Vitesse nulle
+    Serial.print(90-gain*u);
+    Serial.print(90-gain*AcY);
   delay(pause);
  
  
